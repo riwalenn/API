b367ed6a02b4d4ede1318f8c062e2052fb875939
riwalenn <7VdDQCwL3L&M6#q>
2021-09-12 18:08:48 +0200

    [ADD] fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,72 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    private $manager;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->manager = $manager;
+        $this->loadUsers();
+        $this->loadAdminRole();
+        $this->loadUserRole();
 
         $manager->flush();
+    }
+
+    public function loadUsers()
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 1; $i < 5; $i++) {
+            $user = new Users();
+            $username = $faker->userName;
+            $user->setUsername($username)
+                ->setEmail($faker->email)
+                ->setPassword($this->passwordEncoder->encodePassword($user, $username))
+                ->setRoles(['ROLE_USER'])
+                ->setState($faker->boolean)
+                ->setCreatedAt(new \DateTime())
+                ->setModifiedAt(new \DateTime());
+            $this->manager->persist($user);
+        }
+    }
+
+    public function loadAdminRole()
+    {
+        $user = new Users();
+        $user->setUsername('admin')
+                ->setEmail('admin@gmail.com')
+                ->setPassword($this->passwordEncoder->encodePassword($user, "admin"))
+                ->setRoles(['ROLE_ADMIN'])
+                ->setState(1)
+                ->setCreatedAt(new \DateTime())
+                ->setModifiedAt(new \DateTime());
+        $this->manager->persist($user);
+    }
+
+    public function loadUserRole()
+    {
+        $user = new Users();
+        $user->setUsername('user')
+            ->setEmail('user@gmail.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user, "user"))
+            ->setRoles(['ROLE_USER'])
+            ->setState(1)
+            ->setCreatedAt(new \DateTime())
+            ->setModifiedAt(new \DateTime());
+        $this->manager->persist($user);
     }
 }
---
+++ src/DataFixtures/AppPostsFixtures.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Categories;
+use App\Entity\FavoritesPosts;
+use App\Entity\Posts;
+use App\Entity\Users;
+use App\Repository\CategoriesRepository;
+use App\Repository\UsersRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
+
+class AppPostsFixtures extends Fixture implements DependentFixtureInterface
+{
+    private $manager;
+    private $userRepository;
+    private $categoriesRepository;
+
+    public function __construct(UsersRepository $usersRepository, CategoriesRepository $categoriesRepository)
+    {
+        $this->userRepository = $usersRepository;
+        $this->categoriesRepository = $categoriesRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        $this->manager = $manager;
+        $users = $this->userRepository->findAll();
+
+        $category = new Categories();
+        $category->setValue('graphismes, design')
+                    ->setCss('fas fa-fill-drip')
+                    ->setColor('#15aabf');
+        $this->manager->persist($category);
+        $this->manager->flush();
+
+        $category2 = new Categories();
+        $category2->setValue('e-commerce')
+            ->setCss('far fa-credit-card')
+            ->setColor('#be4bdb');
+        $this->manager->persist($category2);
+        $this->manager->flush();
+
+        $category3 = new Categories();
+        $category3->setValue('social media')
+            ->setCss('fas fa-user-friends')
+            ->setColor('#4c6ef5');
+        $this->manager->persist($category3);
+        $this->manager->flush();
+
+        $array = [$category->getId(), $category2->getId(), $category3->getId()];
+
+        foreach ($users as $user) {
+            for ($i = 1; $i < 3; $i++) {
+                $category = array_rand(array_flip($array));
+                $id_category = $this->categoriesRepository->findOneBy(['id' => $category]);
+                $post = new Posts();
+                $post->setTitle($faker->sentence(6, true))
+                    ->setKicker($faker->sentence(9, true))
+                    ->setContent($faker->paragraph(2))
+                    ->setAuthor($user)
+                    ->setCategory($id_category)
+                    ->setCreatedAt(new \DateTime())
+                    ->setModifiedAt(new \DateTime())
+                    ->setState($faker->boolean);
+
+                $this->manager->persist($post);
+
+                for ($j = 1; $j < 3; $j++) {
+                    $favoris = new FavoritesPosts();
+                    $favoris->setPost($post)
+                        ->setUser($user);
+                    $this->manager->persist($favoris);
+                }
+            }
+        }
+
+        $this->manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+          AppFixtures::class,
+        ];
+    }
+}
